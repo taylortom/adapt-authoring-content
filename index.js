abc39faebaf3ce1613c56b240bfb53a54bd2769c
Thomas Taylor <hello@tomtaylor.name>
2023-03-20 13:22:24 +0000

    Enable eslint and standard ruleset

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/** 
+/**
  * Functionality related to course content
  * @namespace content
  */
-export { default } from './lib/ContentModule.js';
+export { default } from './lib/ContentModule.js'
